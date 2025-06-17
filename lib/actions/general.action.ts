7e7db70eac26339b5add1b4a3ad5dacb8e707af3
Tarun Murali <tarunmurali02@gmail.com>
2025-06-17 05:05:47 -0400

    dded envs

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -11,6 +11,24 @@ export async function getInterviewById(idToken: string): Promise<Interview | nul
     return interview.data() as Interview | null;
 
 }
+export async function getFeedbackByInterviewId(
+    params: GetFeedbackByInterviewIdParams
+): Promise<Feedback | null> {
+    const { interviewId, userId } = params;
+
+    const querySnapshot = await db
+        .collection("feedback")
+        .where("interviewId", "==", interviewId)
+        .where("userId", "==", userId)
+        .limit(1)
+        .get();
+
+    if (querySnapshot.empty) return null;
+
+    const feedbackDoc = querySnapshot.docs[0];
+    return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
+}
+
 export async function createFeedback(params: CreateFeedbackParams) {
     const { interviewId, userId, transcript, feedbackId } = params;
     console.log(interviewId);
